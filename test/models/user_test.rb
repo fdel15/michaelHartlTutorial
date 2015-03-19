8aa0294137c22e9cc8db542edfe02b10452fb87c
Frank DelPidio <frankdaedalus@gmail.com>
2015-03-19 13:49:40 -0400

    Tests: fix typo in password length test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,7 +55,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "password should have a minimum length" do
-    @user.password = @password.confirmation = "a" * 5
+    @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 end
