97d515d08efb92d0dbb5cbd2ae596ec932e4c66a
Frank DelPidio <frankdaedalus@gmail.com>
2015-03-19 13:36:32 -0400

    Models: add has_secure_password to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 50 }, format:{ with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false }
+
+  has_secure_password
+
+  #gives us the ability to store a securely hashed password_digest attribute to the database
+  ###What is a password_digest attribute? password_digest is a column in your model that will hold the encrypted version of the password
+
+  #a pair of virtual attributes (password and password_confirmation) including presence validations upon object creation and a validation requiring that they match
+
+  #an authenticate method that returns the user when the password is correct and false otherwise
 end
 
 # What does ActiveRecord::Base contain?
