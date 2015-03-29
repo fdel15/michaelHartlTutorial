4ba129d54e1d1ace2f7383b553c38aee257fd950
Frank DelPidio <frankdaedalus@gmail.com>
2015-03-28 22:28:19 -0400

    Models: create authenticated method for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token
+
   before_save{ email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -20,9 +21,13 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
-   def remember
+  def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
 end
