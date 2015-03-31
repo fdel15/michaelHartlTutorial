0432e2d5c3cb4658d2ce7d8692bdcbb78c9759b7
Frank DelPidio <frankdaedalus@gmail.com>
2015-03-30 22:42:47 -0400

    Helpers: create log_in_as in sessions_helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -37,4 +37,20 @@ module SessionsHelper
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
+
+  def log_in_as(user, options = {})
+    password = options[:password] || |'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: {
+        email: user.email,
+        password: password
+        remember_me: remember_me
+      }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+
 end
