0bf3933e0a4211be5a8b5e72bb485a7e0a7ea116
Frank DelPidio <frankdaedalus@gmail.com>
2015-04-02 18:39:02 -0400

    Test: fix typo

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -46,6 +46,6 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should redirect index when not logged in" do
     get :index
-    assert_redirect_to login_url
+    assert_redirected_to login_url
   end
 end
