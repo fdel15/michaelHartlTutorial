de3e773a42155157470678c3cc0f4731f614e8e6
Frank DelPidio <frankdaedalus@gmail.com>
2015-04-02 12:45:51 -0400

    Helpers: create redirect_back_or helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -42,4 +42,9 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
 end
