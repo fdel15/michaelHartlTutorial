40873131e8277425c4b87d37e51d84e89197c30f
Frank DelPidio <frankdaedalus@gmail.com>
2015-04-06 06:00:43 -0400

    Models: refactor downcase_email to a method reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token
 
-  before_save{ email.downcase! }
+  before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -37,6 +37,10 @@ class User < ActiveRecord::Base
   end
 
   private
+
+  def downcase_email
+    self.email = email.downcase_email
+  end
 
   def create_activation_digest
     self.activation_token = User.new_token
