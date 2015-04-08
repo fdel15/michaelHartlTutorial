f8ddd781ba0127b7bd972b1f0a103fa1970d8b5c
Frank DelPidio <frankdaedalus@gmail.com>
2015-04-08 07:33:47 -0400

    Test: update users_login integration test bc of change to authenticated? method

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -43,7 +43,7 @@ class UsersLogin < ActionDispatch::IntegrationTest
   end
 
   test "authenticated? should return false for user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 
   test "login with remembering" do
