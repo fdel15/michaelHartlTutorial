5688f8b87b74e4b7b5434f314c8e27d71d348dcc
Frank DelPidio <frankdaedalus@gmail.com>
2015-04-09 10:54:26 -0400

    Models: fix typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ActiveRecord::Base
   end
 
   def send_activation_email
-    Usermailer.account_activation(self).deliver_now
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
