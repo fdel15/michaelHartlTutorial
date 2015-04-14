6b7da5b0c563e21d9ede30f7eb04f6e295de9b00
Frank DelPidio <frankdaedalus@gmail.com>
2015-04-13 23:11:13 -0400

    Test: add model tests for micropost

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:frank)
+    @micropost = Micropost.new(content: "Lorem Ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+end
