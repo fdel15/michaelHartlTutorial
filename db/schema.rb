a76b8dcb9e5065fdee75d9fb3e0490e07eee4f07
Frank DelPidio <frankdaedalus@gmail.com>
2015-04-13 23:05:40 -0400

    Migration: create microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408135704) do
+ActiveRecord::Schema.define(version: 20150414030108) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "users_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
