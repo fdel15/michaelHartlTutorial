60323732755a37d2210378487c3eb8c1480e3551
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-04 19:58:48 -0400

    Migration: add picture to microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414030108) do
+ActiveRecord::Schema.define(version: 20150504235701) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
   create_table "users", force: :cascade do |t|
