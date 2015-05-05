8d4a47eb53e3d3fdb06e0f91452280943f7abed7
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-04 21:12:49 -0400

    Uploader: resize images larger than 400px by 400px

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
