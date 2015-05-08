f429c99dc4d1dbd19bf6cb5a96828efab041b5f9
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-08 12:10:37 -0400

    Controllers: create the create action for relationships

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,11 @@
 class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
+  def create
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    redirect_to user
+  end
 
 
 end
